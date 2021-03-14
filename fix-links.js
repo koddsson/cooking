4e83dc02da7853c14b7d6d1b1aab38f2af35372b
Kristján Oddsson <koddsson@gmail.com>
2021-03-14 10:39:06 +0000

    Fix links in the whole file, not just the first instance

--- fix-links.js
+++ fix-links.js
@@ -6,7 +6,7 @@ const markdownPaths = paths.filter((path) => path.endsWith(".md"));
 for (const path of markdownPaths) {
   const data = fs
     .readFileSync(path, { encoding: "utf8" })
-    .replace(/\[\[(.*)\]\]/, function (_, p1) {
+    .replace(/\[\[(.*)\]\]/g, function (_, p1) {
       const url = p1.replace(" ", "%20");
       return `[${p1}](./${url}.md)`;
     });
