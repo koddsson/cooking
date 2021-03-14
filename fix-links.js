8ba3e2b853ee490e2399ba658dfdc904a79843a0
Kristján Oddsson <koddsson@gmail.com>
2021-03-14 10:56:11 +0000

    Return text without obsidian formatting if file doesn't exist

--- fix-links.js
+++ fix-links.js
@@ -6,13 +6,13 @@ const markdownPaths = paths.filter((path) => path.endsWith(".md"));
 for (const path of markdownPaths) {
   const data = fs
     .readFileSync(path, { encoding: "utf8" })
-    .replace(/\[\[(.*)\]\]/g, function (match, p1) {
-      const path = p1.replace(" ", "%20") + ".md";
+    .replace(/\[\[(.*)\]\]/g, function (_, capture) {
+      const path = capture.replace(" ", "%20") + ".md";
       const fileExists = fs.existsSync(path);
       if (fileExists) {
-        return `[${p1}](./${path})`;
+        return `[${capture}](./${path})`;
       }
-      return match;
+      return capture;
     });
   fs.writeFileSync(path, data);
 }
