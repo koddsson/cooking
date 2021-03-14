6f251e28a47ba782f0c863da0eaaed48ba81cd96
Kristján Oddsson <koddsson@gmail.com>
2021-03-14 10:55:17 +0000

    Only create links for files that exist

--- fix-links.js
+++ fix-links.js
@@ -6,9 +6,13 @@ const markdownPaths = paths.filter((path) => path.endsWith(".md"));
 for (const path of markdownPaths) {
   const data = fs
     .readFileSync(path, { encoding: "utf8" })
-    .replace(/\[\[(.*)\]\]/g, function (_, p1) {
-      const url = p1.replace(" ", "%20");
-      return `[${p1}](./${url}.md)`;
+    .replace(/\[\[(.*)\]\]/g, function (match, p1) {
+      const path = p1.replace(" ", "%20") + ".md";
+      const fileExists = fs.existsSync(path);
+      if (fileExists) {
+        return `[${p1}](./${path})`;
+      }
+      return match;
     });
   fs.writeFileSync(path, data);
 }
